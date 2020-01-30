640f8cfe799740be086bd6a0c6b37a76083c7c21
ju <wowow1859@naver.com>
2020-01-30 15:21:57 +0900

    function -> class 로 변경

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+      <div className="gray-background">
+        <img src={logo} lat="logo" />
+        <h2>Let's develop management system!!</h2>
+      </div>
+    );
+  }
 }
+
 
 export default App;
