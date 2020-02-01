c237294f7a7380b52aa39129c6d19b876c882636
ju <wowow1859@naver.com>
2020-02-01 18:04:08 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': '1',
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '주홍범',
-            'birthday': '941018',
-            'gender': '남자',
-            'job': '개발자'
-          },
-          {
-            'id': '2',
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '홍길동',
-            'birthday': '910101',
-            'gender': '남자',
-            'job': '의사' 
-          },
-          {
-            'id': '3',
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '이순신',
-            'birthday': '921515',
-            'gender': '남자',
-            'job': '군인'
-          }
-    ]);
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
